219ed12fb159d1494c8a0f62d3bc085d3de5d605
Rajat Raj Agrahari <rajatagrahari56@gmail.com>
2023-03-18 00:23:09 +0530

    assign

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React from 'react'
-const App = () => {
-    return (
-        <div>
-            Test app page.
+import React from "react";
+// import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-        </div>
+const App=() => {
+    return(
+        <div>start</div>
+        // <BrowserRouter>
+        // <Routes>
+        //     <Route>
+                
+        //     </Route>
+
+
+        // </Routes>
+
+        
+        // </BrowserRouter>
     )
 }
-export default App
+
+export default App;
